d3f08707edef4349668a0397658e8fda8e78ff0b
fadel <fadelsougou@gmail.com>
2020-06-09 14:35:04 +0200

    solving conflicts

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,12 @@
 import React from "react";
-<<<<<<< HEAD
-<<<<<<< HEAD
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import FilmPage from "./components/FilmPage";
-=======
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import PokemonDisplayer from "./components/PokemonDisplayer";
->>>>>>> origin/arthur
-=======
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import FilmPage from "./components/FilmPage";
->>>>>>> 096c9a22418072407ec861d9ad9ee1f7a2ecac60
 import HomePage from "./components/HomePage";
 
 function App() {
   return (
     <Router>
       <div>
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 096c9a22418072407ec861d9ad9ee1f7a2ecac60
         <nav>
           <ul>
             <li>
@@ -31,14 +17,6 @@ function App() {
             </li>
           </ul>
         </nav>
-<<<<<<< HEAD
-=======
-        
->>>>>>> origin/arthur
-=======
->>>>>>> 096c9a22418072407ec861d9ad9ee1f7a2ecac60
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
         <Switch>
           <Route path="/demo">
             <FilmPage />
