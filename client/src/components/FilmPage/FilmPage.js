37c9aabd3598cf53d90dfe8b4c51cd33cda272e2
fadel <fadelsougou@gmail.com>
2020-06-10 14:49:43 +0200

    buttons instead of stars to rate the movies

--- client/src/components/FilmPage/FilmPage.js
+++ client/src/components/FilmPage/FilmPage.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from "react";
-import StarRatingComponent from "react-star-rating";
 import "../MovieList.js";
 import "./FilmPage.css";
-import Rating from "./rating.js";
+
 
 var obj = {name:"Benjamin Button", director:"David Fisher", actors: ["Brad Pitt", "Taraji P. Henson", "Tilda Swinton"], year:"2008"};
+const img_url = "https://img.aws.la-croix.com/2017/01/08/1100815592/Lhistoire-homme-vieillard-meurt-nourrissonavoir-rebours_0_730_489.jpg"
+var rate = 0
+
 
 const FilmPage = () => {
   const [error, setError] = useState(null);
@@ -33,32 +35,37 @@ const FilmPage = () => {
     // The useEffect hook will retrigger every time an element in the dependency array changes.
     // changes = strict egality, so beware when mutating objects
   }, [fetchAgain]);
-
   const Film = () => {
+    
     if (error) {
       return <div>Error: {error.message}</div>;
     } else if (!isLoaded) {
       return <div>Loading...</div>;
     } else {
+      
       return (
         <div>
-          <link rel="stylesheet" type="text/css" href="FilmPage.css"/>
-          
           <div class = "hero">
+            <link rel="stylesheet" type="text/css" href="FilmPage.css"/>
             <h2>{obj.name}</h2>
-            <img src = "https://img.aws.la-croix.com/2017/01/08/1100815592/Lhistoire-homme-vieillard-meurt-nourrissonavoir-rebours_0_730_489.jpg"/>
+            <img src = {img_url}/>
             <h1>produced in {obj.year} by {obj.director}</h1>
             <h1> Actors </h1>
             <ul>
               {obj.actors.map((actor) => <li>{actor}</li>)}
             </ul>
+            <h1>How did you find {obj.name} ?</h1>
+            <button class="btn Vbad" onClick = {rate = 1}>Very Bad</button>
+            <button class="btn bad" onClick = {rate = 2}>Bad</button>
+            <button class="btn fair" onClick = {rate = 3}>Fair</button>
+            <button class="btn good" onClick = {rate = 4}>Good</button>
+            <button class="btn excellent" onClick = {rate = 5} >Excellent</button> 
           </div>
-          <Rating/>
         </div>
       );
     }
   };
-
+            
   return (
     <div>
       <button onClick={triggerFetchAgain}>Fetch again</button>
