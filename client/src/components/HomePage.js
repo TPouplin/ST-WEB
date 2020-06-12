158f8382095d6ed18853eb57f2c95fbadfbf4328
Arthur Guédon <arthur.guedon@student-cs.fr>
2020-06-12 11:09:35 +0200

    ajoute plusieurs recommendations

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -6,17 +6,16 @@ import "./HomePage.css";
 import "./Button.css"
 const HomePage = () => {
 
-  const [recommendation,setRecommendation] = useState(null)
+  const [recommendations,setRecommendations] = useState(null)
   const [displayRecommendation,setDisplayRecommendation] = useState(true)
   //Fonction pour recevoir une recommendation 
   const getRecommendation = async ()=>{
     const response = await fetch("https://nrxfc2lxz1.execute-api.eu-west-1.amazonaws.com/dev/recommandation",{method:"POST",body:JSON.stringify({user_id:localStorage.getItem('pseudo')})})
     const responseJson = await response.json()
     console.log(responseJson)
-    if(responseJson.length>0) {
-      setRecommendation(responseJson[0])
+  
+      setRecommendations(responseJson)
       setDisplayRecommendation(false)
-    }
   }
 
   return (
@@ -25,9 +24,11 @@ const HomePage = () => {
           {displayRecommendation && (<button className="button" onClick={getRecommendation}>Recevoir une recommendation</button>)}
           
           
-          {recommendation && (<div>
-            <h2 className="recommendation-title">Film recommendé</h2>
-            <div className="recommendation-container" key={recommendation.uuid}>
+          {recommendations && (<div>
+            
+            <h2 className="recommendation-title">Films recommendés</h2>
+            {recommendations.map((recommendation)=>(
+              <div className="recommendation-container" key={recommendation.uuid}>
               <Link className="list-link"  to={{pathname:'/movie/'+recommendation.uuid,state:{movie:recommendation}}}>{recommendation.name}</Link>
               <p>{recommendation.date}</p>
               <div className="movie-genres">
@@ -36,6 +37,9 @@ const HomePage = () => {
                   ))}
             </div>
           </div>
+
+            ))}
+            
           </div>
           )
         } 
